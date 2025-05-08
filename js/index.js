7afd502470ca8e358afc3ee1b3266e7c501a747f
jaipearl27 <jaix2793@gmail.com>
2025-05-08 12:41:50 +0530

    updates

--- js/index.js
+++ js/index.js
@@ -50,7 +50,7 @@ function Navbar(targetId = "navbar") {
     </div>
 
     <!-- Get Started Button (always visible) -->
-    <a href="https://app.assetmanagment.in" class="bg-orange-400 text-white px-4 py-2 rounded-md hidden md:inline-block">
+    <a href="https://app.assetmanagment.in" class="bg-orange-400 text-white px-4 py-2 rounded-md hidden xl:inline-block">
       Get Started
     </a>
 
